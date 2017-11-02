c2e21ee3319394a50b20e2173dfe2e78bba3871b
Ankit Srivastava <asrivast28@gmail.com>
2017-11-02 17:39:31 -0400

    Provide options for degree distribution.

--- main.py
+++ main.py
@@ -15,11 +15,22 @@ def parse_args():
     parser.add_argument('--vertices', '-v', metavar = 'V', type = int, default = 1000, help = 'total number of vertices')
     parser.add_argument('--sources', '-s', metavar = 'S', type = int, help = 'number of sources')
     parser.add_argument('--targets', '-t', metavar = 'T', type = int, help = 'number of targets')
+    parser.add_argument('--degreedist', '-d', metavar = 'D', type = int, default = 1, help = 'degree distribution: 1- 1 + Poisson(1), 2- Constant(2), 3- 1 + Poisson(3)')
     parser.add_argument('--alpha', '-a', metavar = 'A', type = float, default = 0.0, help = 'preference for reuse')
     parser.add_argument('--tau', '-u', metavar = 'U', type = float, default = 0.9, help = 'path coverage threshold')
     parser.add_argument('--seed', '-n', metavar = 'N', type = int, default = 0, help = 'seed for the PRNG')
 
     args = parser.parse_args()
+    # In-degree distribution for the network
+    if args.degreedist == 1:
+        args.d_in = lambda : 1 + np.random.poisson(1.0)
+    elif args.degreedist == 2:
+        args.d_in = lambda : 2
+    elif args.degreedist == 3:
+        args.d_in = lambda : 1 + np.random.poisson(3.0)
+    else:
+        raise argparse.ArgumentTypeError, 'degree distribution should be one of {1, 2, 3}'
+
     return args
 
 
@@ -38,10 +49,8 @@ def main():
     if args.file:
         G = network.read(args.file)
     else:
-        # In-degree distribution for the network
-        d_in = lambda : 1 +_np.random.poisson(1.0)
         intermediates = args.vertices - (args.sources + args.targets)
-        G = network.rp_model(args.sources, intermediates, args.targets, args.alpha, d_in)
+        G = network.rp_model(args.sources, intermediates, args.targets, args.alpha, args.d_in)
 
     # Get the core vertices for the network
     C = analysis.core_vertices(G, args.tau)
