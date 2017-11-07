c90cb3e3483da7e26e4c8f8468d43af64c16d9fd
Ankit Srivastava <asrivast28@gmail.com>
2017-11-06 20:42:57 -0500

    Avoid recomputing all the path centralities in every iteration.

--- analysis.py
+++ analysis.py
@@ -4,24 +4,25 @@ import numpy as np
 import utils
 
 
-def path_centrality(G, P_s, P_t, centrality):
+def update_path_centrality(G, P_s, P_t, centrality, vertex=None):
     """
-    @brief  Computes path centrality values for all the vertices in a network.
+    @brief  Updates path centrality values for the vertices in a network.
 
     @param G           nx.DiGraph or nx.MultiDiGraph representation of the network.
     @param P_s         NumPy array with 1 for every source vertex and 0 for other vertices.
     @param P_s         NumPy array with 1 for every target vertex and 0 for other vertices.
     @param centrality  NumPy array containg centrality value for every vertex in the given network.
+    @param vertex      Only update successors and predecessors of the vertex, if it is not None.
     """
     # Compute the number of paths from sources to every vertex: complexity
     first_level = set()
-    for s in np.where(P_s)[0]:
+    for s in np.where(P_s)[0] if vertex is None else [vertex]:
         first_level.update(n for n in utils.forward_iter(G, s))
     utils.count_simple_paths(G, utils.reverse_iter, utils.forward_iter, first_level, P_s)
 
     # Compute the number of paths from every vertex to targets: generality
     first_level = set()
-    for t in np.where(P_t)[0]:
+    for t in np.where(P_t)[0] if vertex is None else [vertex]:
         first_level.update(n for n in utils.reverse_iter(G, t))
     utils.count_simple_paths(G, utils.forward_iter, utils.reverse_iter, first_level, P_t)
 
@@ -74,13 +75,13 @@ def core_vertices(G, source, target, tau, datatype=np.uint64):
     in_degree = np.array(list(G.in_degree(n) for n in xrange(G.number_of_nodes())), dtype=datatype)
     out_degree = np.array(list(G.out_degree(n) for n in xrange(G.number_of_nodes())), dtype=datatype)
 
-    # Compute initial path centralities
     # Initialize complexity for the source vertices to 1
     P_s = source.astype(datatype)
     # Initialize generality for the target vertices to 1
     P_t = target.astype(datatype)
+    # Compute initial path centralities
     centrality = np.empty(source.size, datatype)
-    path_centrality(G, P_s, P_t, centrality)
+    update_path_centrality(G, P_s, P_t, centrality)
 
     # Compute the initial number of S-T paths
     # Same as the number of paths exiting from the sources
@@ -102,12 +103,12 @@ def core_vertices(G, source, target, tau, datatype=np.uint64):
             for vertex in candidate_vertices:
                 _source = np.copy(source)
                 _target = np.copy(target)
+                _P_s = np.copy(P_s)
+                _P_t = np.copy(P_t)
+                _P_s[vertex] = 0
+                _P_t[vertex] = 0
+                update_path_centrality(G, _P_s, _P_t, centrality, vertex)
                 vertex_edges = remove_vertex(G, vertex, _source, _target, np.copy(in_degree), np.copy(out_degree))
-                P_s[source] = 1
-                P_s[~source] = 0
-                P_t[target] = 1
-                P_t[~target] = 0
-                path_centrality(G, P_s, P_t, centrality)
                 neighbors = set([vertex])
                 for other in candidate_vertices:
                     if other != vertex and centrality[other] == 0:
@@ -125,15 +126,11 @@ def core_vertices(G, source, target, tau, datatype=np.uint64):
                     updates = False
             candidate_vertex = PES[0]
         for vertex in candidate_vertex:
+            P_s[vertex] = 0
+            P_t[vertex] = 0
+            update_path_centrality(G, P_s, P_t, centrality, vertex)
             remove_vertex(G, vertex, source, target, in_degree, out_degree)
         C.append(candidate_vertex if len(candidate_vertex) > 1 else candidate_vertex.pop())
-
-        # Recompute path centralities
-        P_s[source] = 1
-        P_s[~source] = 0
-        P_t[target] = 1
-        P_t[~target] = 0
-        path_centrality(G, P_s, P_t, centrality)
 
         P_R = P - np.sum(source * centrality)
     return C
